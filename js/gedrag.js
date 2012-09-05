29fac7635427a6d77b1a305b03275cf254f2cb49
DrummerHead <drummerhead@gmail.com>
2012-09-05 14:46:47 -0300

    Take url and put it into the dom

--- js/gedrag.js
+++ js/gedrag.js
@@ -1,5 +1,5 @@
+/*
 jQuery(function($){
-
 
 
 
@@ -39,12 +39,70 @@ vistaLoca.render();
 $('#alphaTrigger').append(vistaLoca.el);
 
 
-
-
-
-
-
-
-
-
 });
+
+*/
+
+var Amego = Backbone.Model.extend({
+  defaults: {
+    "nomen": "Sheison"
+  },
+
+  changeNomen: function(newNomen){
+    this.set({nomen: newNomen});
+  }
+});
+
+
+var AmegoView = Backbone.View.extend({
+  tagName: "h1",
+
+  initialize: function(){
+    this.model.on("change", this.setNomen, this);
+  },
+
+  setNomen: function(newNomen){
+    this.$el.text(this.model.get("nomen"));
+  },
+
+  render: function(){
+    this.setNomen();
+
+    return this;
+  }
+});
+
+
+var CositoApp = Backbone.Router.extend({
+  routes:{
+    ":magia": "magia"
+  },
+
+  initialize: function(){
+    this.amego = new Amego;
+    var amegoView = new AmegoView({model: this.amego});
+
+    $('body').html(amegoView.render().el);
+  },
+
+  magia: function(magia){
+    this.amego.changeNomen(magia);
+  },
+
+  start: function(){
+    //Backbone.history.start({pushState: true});
+    Backbone.history.start();
+  }
+});
+
+
+
+
+jQuery(function($){
+  cosito = new CositoApp;
+  cosito.start();
+})
+
+
+
+
